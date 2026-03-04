3e2fc8196cd2c9ffb3cd8d40c28b0380901f2a9e
zayanvali <zayan.vali@icloud.com>
2026-03-04 18:34:58 +0000

    Add files via upload

--- Pandas.py
+++ Pandas.py
@@ -1,4 +1,14 @@
 import pandas as pd
-data  = pd.read_csv("AirQualityUCI.csv")
+data  = pd.read_csv("Titanic-Dataset.csv")
 print(data.head())
 print(data.info())
+name_and_age = data[["Name", "Age", "Survived"]]
+print(name_and_age.head())
+above_25 = data[data["Age"]>25]
+print(above_25.head())
+above_25_new = name_and_age[name_and_age["Age"]>25]
+print(above_25_new.head())
+class_2_or_3 = data[(data["Pclass"]== 2)|(data["Pclass"]== 3)]
+print(class_2_or_3[["Name", "Age", "Pclass"]].head())
+male_1 = data[(data["Sex"]== "male")&(data["Pclass"]== 1)]
+print(male_1.head())
