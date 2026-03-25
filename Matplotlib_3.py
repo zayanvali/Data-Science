33f713348ceaab89c6beaad75c108ab0d99d1c99
zayanvali <zayan.vali@icloud.com>
2026-03-25 18:25:18 +0000

    Update Matplotlib_3.py

--- Matplotlib_3.py
+++ Matplotlib_3.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 data  = pd.read_csv("Titanic-Dataset.csv")
-acsending_fare = data.sort_values(by = "Fare", ascending = True).head(30)
-acsending_fare = acsending_fare.reset_index(drop = True)
-actual_top = data.sort_values(by = "Fare", ascending = False)
-actual_top = acsending_fare.reset_index(drop = True)
-plt.plot(actual_top, actual_top["Fare"], marker = "o", linestyle = "-", color = "b")
+actual_top = data.sort_values(by = "Fare", ascending = False).head(30)
+actual_top = actual_top.reset_index(drop = True)
+plt.plot(actual_top.index, actual_top["Fare"], marker = "o", linestyle = "-", color = "b")
 plt.title("Highest Titanic Fare")
 plt.xlabel("PassengerIndex 0-29")
 plt.ylabel("Fare Value")
