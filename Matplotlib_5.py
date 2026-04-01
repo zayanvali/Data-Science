711d58d00e16a90f76b9388614acbcb4f595f341
zayanvali <zayan.vali@icloud.com>
2026-04-01 18:12:01 +0100

    Update Matplotlib_5.py

--- Matplotlib_5.py
+++ Matplotlib_5.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 data  = pd.read_csv("Titanic-Dataset.csv")
-x = data.sort_values(by = "Age", ascending = True).head(30)
-y = data[["SibSp"]]
+youngest = data.sort_values(by = "Age", ascending = True).head(30)
+y = youngest["Age"]
+x = youngest["SibSp"]
 plt.plot(x, y, "b-")
 plt.axis([0, 30, 0, 4])
 plt.xlabel("x-axis")
